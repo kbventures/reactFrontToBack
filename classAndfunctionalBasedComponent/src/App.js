98eb01945e06f474270b48aa0d7800e6e485c297
kbventures <kmb_x@hotmail.com>
2020-11-25 19:44:16 +0200

    HTTP Request & Updating State

--- classAndfunctionalBasedComponent/src/App.js
+++ classAndfunctionalBasedComponent/src/App.js
@@ -4,81 +4,46 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users'
 
 import './App.css';
+import axios from 'axios';
 
 
 // Class based component
-//
-// Within JSX we use className,
-// otherwise we get Invalid DOM property...
-// error
-//
-// Within JSX we use htmlFor instead of for
-// for avoiding erros like above
-//
-// In jsx you need one parent elment
-// otherwie you will get errors(ie:div);
-//
-// If we don't want those div's to show
-// up in the dom we can use <React.Fragment>
-// </React.Fragment> has wrappers instead of div's
 
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//     <div className="App">
-//       <h1>Hello</h1>
-//     </div>
-//     );
-
-//   }
-// }
-
-
-// class App extends Component {
-//   render() {
-//     return React.createElement(
-//       'div',
-//       { className: 'App'},
-//       React.createElement('h1',null, 'Hello World')
-//     );
-//   }
-// }
-
-
-// expression examples
-
-// class App extends React.Component {
-
-//   fooPartOfClass = () => 'Barssss';
-
-//   render() {
-//     const name = 'John Doe';
-//     const foo = () => 'bar';
-//     const loading = false;
-//     const showName = true;
-//     return (
-//       <div className="App">
-//         <h1>Hello {name.toUpperCase()}</h1>
-//         <h1>Hello {1 + 1}</h1>
-//         <h1>Hello {foo()}</h1>
-//         <h1>Hello {this.fooPartOfClass()}</h1>
-//         {loading ? <h4>Loading...</h4> : <h1>Hello {showName ? name : null}</h1>}
-//         {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
-//       </div>
-//     );
-//   }
-// }
-
-// Props
 
 class App extends React.Component {
+
+  state = {
+    users: [],
+    loading: false
+  }
+
+  // Render and componentDidMount are lifecyle method
+
+  async componentDidMount(){
+
+    // we can't change the state this way
+    // this.state.loading = true; for class components
+
+    /*
+    You can and should use this.setState() in only these React lifecycle
+    methods: componentDidMount, componentDidUpdate and 
+    componentWillReceiveProps. You can also set it in the componentWillMount
+    method, but it’s recommend to use the constructor instead.
+    */
+
+    this.setState({loading:true});
+
+    const res = await axios.get('https://api.github.com/users');
+    
+    this.setState({users: res.data, loading: false});
+  }
+
   render() {
     return (
       <div className='App'>
        <Navbar title='Github Finder' icon='fab fa-github'/>
        <div className="container">
-        <Users />
+        <Users loading={this.state.loading} users={this.state.users} />
        </div>
       </div>
     );
